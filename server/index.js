7948b8aafc8878fe90b0edc2fcadb92ab81036b0
Ankit9442 <ankitrawat20052001@gmail.com>
2023-02-06 21:08:41 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
-
+// heyy
 app.get('/', (req, res) => res.send('Hello World! from server'));
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
