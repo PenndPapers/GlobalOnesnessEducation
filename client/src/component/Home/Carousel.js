9e19006eee2b28e71660ba21ee9c0728bd94cdd0
Ankit9442 <ankitrawat20052001@gmail.com>
2023-02-10 22:38:59 +0530

    carousel created

---
+++ client/src/component/Home/Carousel.js
@@ -0,0 +1,48 @@
+import React , {useState , useEffect } from 'react'
+import {CgChevronLeft , CgChevronRight} from 'react-icons/cg';
+
+const slides = [
+    {
+        url : "https://allenwebsite-general.s3.ap-south-1.amazonaws.com/allen-website/slider/top-slider/onlinecourse-allendigital.jpg"
+    },
+    {
+        url : "https://allenwebsite-general.s3.ap-south-1.amazonaws.com/allen-website/slider/top-slider/allenace-jaipur.jpg"
+    },
+    {
+        url : "https://allenwebsite-general.s3.ap-south-1.amazonaws.com/allen-website/slider/top-slider/jee-main-enthuse-course13-Feb.jpg"
+    },
+    {
+        url : "https://allenwebsite-general.s3.ap-south-1.amazonaws.com/allen-website/slider/top-slider/neet-enthuse-course-14-Feb.jpg"
+    }
+]
+
+const Carousel = () => {
+
+  const [slideNumber , setSlideNumber] = useState(0);
+
+  const preSlide = ()=>{
+    const isFirstSlide = slideNumber === 0 ;
+    const newSlide = isFirstSlide ? slides.length -1 : slideNumber -1;
+    setSlideNumber(newSlide );
+  }
+  const nextSlide = ()=>{
+    setSlideNumber((slideNumber + 1)% slides.length )
+  }
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+          setSlideNumber((slideNumber + 1)% slides.length )  // this  line will run afgter 3 sec 
+    }, 3000);
+    return () => clearTimeout(timer);
+  } , [slideNumber]);
+
+  return (
+    <div className='max-w-[1400px] h-[400px] w-full m-auto   bg-slate-500 relative group '> 
+       <div style={{backgroundImage : `url(${slides[slideNumber].url})`}} className="w-full h-full bg-center bg-cover duration-700   " > </div>
+       <div  className=' hidden group-hover:block  absolute top-[50%] -translate-x-0   translate-y-[-50%] left-5  rounded-full bg-slate-100 ' onClick={preSlide} > <CgChevronLeft  size={50}/> </div>
+      <div    className=' hidden group-hover:block  absolute top-[50%] -translate-x-0    translate-y-[-50%] right-5 rounded-full bg-slate-100 ' onClick={nextSlide}  >  <CgChevronRight  size={50} /> </div>
+     </div>
+  )
+}
+
+export default Carousel
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import Carousel from '../../component/Home/Carousel'
 import NavBar from '../../component/NavBar/NavBar'
 
 const home = () => {
   return (
     <>
       <NavBar/>
-      <div>apply now</div>
-      <div>slider</div>
-
+      <div className="flex flex-row justify-center items-center ">
+        <h1 className="lg:text-5xl lg:font-bold  text-2xl font-normal font-[Roboto] text-[var(--colorPrimary)] mt-8" >
+          10 Day Free Classes!!
+        </h1>
+        <button className=" btn hover:bg-transparent bg-[var(--buttonBlue)] font-[Poppins]  text-white mt-8 ml-8 hover:text-[var(--buttonBlue)]  font-semiboldtext-white py-2 px-3  border-[var(--buttonBlue)] border-2 hover:border-[var(--colorPrimary)] rounded">
+          Apply Now
+        </button>
+      </div>
+      <span className="flex flex-row mt-2 justify-center items-center text-[var(--grayDark)] ">Our result speaks about us !</span>
+      <Carousel/>
+      
     </>
   )
 }
