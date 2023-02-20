f6282b68f6a2649ac68936d0adbfe4fdd742730e
Ankit9442 <ankitrawat20052001@gmail.com>
2023-02-20 18:43:36 +0530

    added footer and about

---
+++ client/src/pages/Home/About.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import p from "../../images/pawan_sir.jpeg" 
+
+
+const About = () => {
+    return (
+        <section className='w-5/6 mx-auto mt-16 md:mt-32 mb-10  '>
+            <h1 className='text-xl sm:text-4xl my-5 '> know about <span className='font-bold '>GLOBAL ONENESS </span> </h1>
+            <div className='sm:flex sm:flex-row  justify-between  items-center  '>
+                <div className='sm:order-2 flex justify-center    ' >
+                    <img className='    h-[200px] w-[200px]  m-5   sm:w-[300px] sm:h-[300px]  ' src={p} />
+                </div>
+                <div className=' flex flex-col gap-5 w-full  sm:w-[60%]   '>
+                    <span>  At Global Oneness, we strive to foster a strong foundation of knowledge and concepts in students, in order to help them achieve their career goals. Our highly qualified and experienced faculty are devoted to providing students with the best academic support and personal care, as well as a platform for the preparation of competitive exams and board level education. We are dedicated to helping our students achieve success in JEE (Main+Advanced), JEE (Main), Pre-Medical (NEET-UG), Pre-Nurture & Career Foundation (Class VI to X, NTSE & Olympiads) and Commerce Education (11th, 12th, CA & CS). We also strive to provide our students with value-based career education, abundant resources, and individual attention, while also being responsible to the parents and society to nurture ethical and responsible career leadership in the children, and to provide a lifelong connection to ethics and excellence in global leaders.</span>
+
+                    <span>  We provide specialized courses for students of classes 11 and 12, to help them prepare for their board exams. Our experienced faculty members guide and mentor the students, to ensure they are well-prepared to face the challenges of the board exams. We also provide personalized attention to each student, so that every student can excel in their respective fields. We strive to provide our students with the best resources and guidance, to ensure their success in the board exams.</span>
+                </div>
+
+            </div>
+        </section>
+    )
+}
+
+export default About
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -5,7 +5,10 @@ import Carousel from '../../component/Home/Carousel'
 import teamglobal from '../../images/teamglobaloneness.jpg'
 import promotion1 from '../../images/promotionglobaloneness.jpg'
 import promotion2 from '../../images/promotion2globaloneness.jpg'
+import About from './About'
+import Footer from '../../component/Footer'
 import { Link } from 'react-router-dom'
+import Teachers from '../../component/Teachers'
 const home = () => {
 
   const slides = [
@@ -21,18 +24,38 @@ const home = () => {
         <h1 className="lg:text-5xl lg:font-bold  text-2xl font-normal font-[Roboto] text-[var(--colorPrimary)] mt-8" >
           10 Day Free Classes!!
         </h1>
-        <Link to= '../studentRegister'>
-        <button className=" btn hover:bg-transparent bg-[var(--buttonBlue)] font-[Poppins]  text-white mt-8 ml-8 hover:text-[var(--buttonBlue)]  font-semiboldtext-white py-2 px-3  border-[var(--buttonBlue)] border-2 hover:border-[var(--colorPrimary)] rounded">
-          Apply Now
-        </button>
+        <Link to='../studentRegister'>
+          <button className=" btn hover:bg-transparent bg-[var(--buttonBlue)] font-[Poppins]  text-white mt-8 ml-8 hover:text-[var(--buttonBlue)]  font-semiboldtext-white py-2 px-3  border-[var(--buttonBlue)] border-2 hover:border-[var(--colorPrimary)] rounded">
+            Apply Now
+          </button>
         </Link>
       </div>
       <span className="flex flex-row mt-2 justify-center items-center text-[var(--grayDark)] ">Our result speaks about us !</span>
 
       <div>
         <Carousel />
+        <section className='bg-gradient-to-r from-blue-700 to-blue-400 h-[150px] flex justify-around items-center  text-white  font-semibold text-center text-xl  '>
+          <div>
+           <span> 1000+ </span>
+            <div>   Student Taught  </div>
+          </div>
+          <div>
+          <span> Best </span>
+          <div>   Result  </div>
+          </div>
+          <div>
+          <span> 1000+ </span>
+          <div>   Student Taught  </div>
+           
+          </div>
+
+        </section>
+        <Teachers />
       </div>
 
+      <About />
+
+      <Footer />
     </>
   )
 }
